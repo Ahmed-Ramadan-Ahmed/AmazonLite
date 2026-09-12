22fa98fcaf4d5350826721f9585f4c020f90707e
Ahmed <aramadan442000@gmail.com>
2026-09-12 18:28:10 +0300

    feat(auth): AuthServices -> JWT

---
+++ auth-service/src/main/java/com/amazonlite/auth/service/JwtService.java
@@ -0,0 +1,54 @@
+package com.amazonlite.auth.service;
+
+import com.amazonlite.auth.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.access-token-expiration-ms}")
+    private long jwtExpiration;
+
+    @Value("${jwt.refresh-token-expiration-ms}")
+    private long refreshExpiration;
+
+    public String generateAccessToken(User user) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("role", user.getRole().name());
+        extraClaims.put("userId", user.getId().toString());
+
+        return buildToken(extraClaims, user.getEmail(), jwtExpiration);
+    }
+
+    public String generateRefreshToken(User user) {
+        return buildToken(new HashMap<>(), user.getEmail(), refreshExpiration);
+    }
+
+    private String buildToken(Map<String, Object> extraClaims, String email, long expiration) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
